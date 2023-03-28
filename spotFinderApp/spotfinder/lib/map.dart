3d3ff8e5f1ecd10d576862551e00846a89a6be61
WoutVanBoxem <woutvanboxem02@gmail.com>
2023-03-28 15:30:09 +0200

    kaart toegevoegd aan app

---
+++ spotFinderApp/spotfinder/lib/map.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MyMap extends StatefulWidget {
+  @override
+  _MyMapState createState() => _MyMapState();
+}
+
+class _MyMapState extends State<MyMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Map')),
+      body: _buildMap(),
+    );
+  }
+
+  Widget _buildMap() {
+    return FlutterMap(
+      options: MapOptions(
+        center:
+            LatLng(40.7128, -74.0060), // Set the location you want to center on
+        zoom: 13.0,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+        ),
+      ],
+    );
+  }
+}
