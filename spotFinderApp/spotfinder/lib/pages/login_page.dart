e927fa9cfc57c7cdf1f4dca1a6d1a7d00a4fb6a5
WoutVanBoxem <woutvanboxem02@gmail.com>
2023-05-02 16:05:26 +0200

    conflicts opgelost

--- spotFinderApp/spotfinder/lib/pages/login_page.dart
+++ spotFinderApp/spotfinder/lib/pages/login_page.dart
@@ -3,111 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:spotfinder/pages/home_page.dart';
 import 'signup_page.dart';
 
-/*
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  late String _email, _password;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log In'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
-                onSaved: (input) => _email = input!,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              TextFormField(
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-                onSaved: (input) => _password = input!,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-              ),
-              ElevatedButton(
-                onPressed: login,
-                child: const Text('Log In'),
-              ),
-              TextButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignUpPage()),
-                ),
-                child: const Text('Sign Up'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void login() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      try {
-        var querySnapshot = await FirebaseFirestore.instance
-            .collection('users')
-            .where('email', isEqualTo: _email)
-            .where('password', isEqualTo: _password)
-            .get();
-
-        if (querySnapshot.docs.length == 1) {
-          // successful login
-          Navigator.pop(context);
-        } else {
-          // invalid email or password
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Invalid Email or Password'),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: const Text('OK'),
-                  ),
-                ],
-              );
-            },
-          );
-        }
-      } catch (e) {
-        print(e);
-      }
-    }
-  }
-}*/
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -130,24 +25,25 @@ class _LoginPageState extends State<LoginPage> {
       if (users.isNotEmpty) {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => const HomePage()),
+          MaterialPageRoute(
+              builder: (context) => HomePage(userEmail: users[0]['email'])),
         );
       } else {
         // invalid email or password
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Invalid Email or Password'),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: const Text('OK'),
-                  ),
-                ],
-              );
-            },
-          );
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Invalid Email or Password'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
       }
     } catch (e) {
       print('Error during login: $e');
